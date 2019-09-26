5bb9a9659eaf71a9cc369b0b2dd09b48cccc8887
Yun <melongbob@gmail.com>
2019-09-25 23:34:47 -0700

    display id on cells

--- src/components/PokemonCell.js
+++ src/components/PokemonCell.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './styles/PokemonCell.css';
 
-const PokemonCell = () => {
+const PokemonCell = ({pokemonClass}) => {
+    const{ id, backgroundPosition } = pokemonClass;
+    
+    const style = {
+        backgroundImage: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png', 
+        backgroundPosition
+    };
+    
     return(
-        <button className = "pokemon-cell">
+        <button style={style} className="pokemon-cell">
+            <h1>{id}</h1>
         </button>
-    );
+    )
 }
 
 export default PokemonCell;
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,35 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
 import PokemonCell from './PokemonCell';
+import { pokemonClasses } from '../pokemonClasses'
 import './styles/PokemonList.css';
 
-class PokemonList extends Component {
-    state = {
-        url:'https://pokeapi.co/api/v2/pokemon?offset=21&limit=21',
-        pokemon: null
-    }
+const cells = pokemonClasses.map(pokemonClass => 
+    <PokemonCell 
+        key={ pokemonClass.id }
+        pokemonClass={ pokemonClass } />)
 
-    async componentDidMount() {
-        const res = await axios.get(this.state.url);
-        this.setState({ pokemon: res.data['results'] });
-    }
+const PokemonList = () => {
 
-    render(){
-        return(
-            <div>
-            {this.state.pokemon ? (            
-                <section className = "pokemon-list">
-                    {this.state.pokemon.map(pokemon => <PokemonCell />)}
-                </section>
-                ) : 
-                (
-                    <h1>Loading Pokemons</h1>
-                ) 
-            }
-            </div>
-
-        )
-    }
+    return(
+        <section className="pokemon-list">
+            {cells}
+        </section>
+    )
 }
 
 export default PokemonList;
